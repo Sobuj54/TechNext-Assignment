a2760c9169a10cd8aa378a9228f704d0280b15dc
soubj <sobujahmed124@gmail.com>
2024-02-11 13:36:57 +0600

    added meta data to single user page

--- src/pages/SingleUser/SingleUser.jsx
+++ src/pages/SingleUser/SingleUser.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Spinner from "../../components/Spinner";
 import MiniHero from "../../components/MiniHero";
+import { Helmet } from "react-helmet-async";
 
 const SingleUser = () => {
   const [user, setUser] = useState("");
@@ -20,46 +21,56 @@ const SingleUser = () => {
   }
 
   return (
-    <section>
-      <MiniHero title="User" subTitle="Users > User" />
+    <>
+      <Helmet>
+        <title>{user.firstName} | UserHub</title>
+        <meta
+          name="description"
+          content={`currently viewing ${user.firstName}`}
+        />
+        <link rel="canonical" href={`/user/${user.id}`} />
+      </Helmet>
+      <section>
+        <MiniHero title="User" subTitle="Users > User" />
 
-      <div className="mx-auto my-10 md:my-20 max-w-7xl">
-        <div className="flex flex-col items-center gap-10 md:flex-row md:gap-0 justify-evenly">
-          {/* image part */}
-          <div>
-            <img src={user.image} alt={user.firstName} />
-          </div>
+        <div className="mx-auto my-10 md:my-20 max-w-7xl">
+          <div className="flex flex-col items-center gap-10 md:flex-row md:gap-0 justify-evenly">
+            {/* image part */}
+            <div>
+              <img src={user.image} alt={user.firstName} />
+            </div>
 
-          {/* description text */}
-          <div className="flex flex-col gap-8">
-            <h1 className="text-3xl font-medium md:text-5xl">
-              {user.firstName} {user.lastName}
-            </h1>
+            {/* description text */}
+            <div className="flex flex-col gap-8">
+              <h1 className="text-3xl font-medium md:text-5xl">
+                {user.firstName} {user.lastName}
+              </h1>
 
-            <div className="flex items-center gap-5 overflow-hidden md:gap-10">
-              <div className="space-y-3">
-                <p>Email </p>
-                <p>Street </p>
-                <p>City </p>
-                <p>Company </p>
-              </div>
-              <div className="mr-0 space-y-3 text-center">
-                <p>:</p>
-                <p>:</p>
-                <p>:</p>
-                <p>:</p>
-              </div>
-              <div className="space-y-3">
-                <p>{user.email}</p>
-                <p> {user.address.address}</p>
-                <p> {user.address.city}</p>
-                <p> {user.company.name}</p>
+              <div className="flex items-center gap-5 overflow-hidden md:gap-10">
+                <div className="space-y-3">
+                  <p>Email </p>
+                  <p>Street </p>
+                  <p>City </p>
+                  <p>Company </p>
+                </div>
+                <div className="mr-0 space-y-3 text-center">
+                  <p>:</p>
+                  <p>:</p>
+                  <p>:</p>
+                  <p>:</p>
+                </div>
+                <div className="space-y-3">
+                  <p>{user.email}</p>
+                  <p> {user.address.address}</p>
+                  <p> {user.address.city}</p>
+                  <p> {user.company.name}</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
